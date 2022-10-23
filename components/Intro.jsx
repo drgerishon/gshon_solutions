6655b62aa2569359c9509be51e7fe13a84f2e909
drgerishon <37508662+drgerishon@users.noreply.github.com>
2022-10-23 18:29:25 +0300

    2ND UPDATE

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,24 +1,26 @@
-/* eslint-disable @next/next/no-img-element */
-import React from 'react';
-import styles from '../styles/intro.module.css';
-import Image from 'next/image';
-import Circle from './Circle';
+import style from "../styles/Intro.module.css";
+import Image from "next/image";
+import Circle from "./Circle";
 
 const Intro = () => {
   return (
-    <div className={styles.container}>
-      <Circle backgroundColor="#b0ff49" top="-50vh" left="-50vh" />
-      <Circle backgroundColor="#01c686" right="-40vh" />
-      <div className={styles.card}>
-        <h1 className={styles.title}>
-          <span className={styles.brand}>GSHON</span> Web Solutions
+    <div className={style.container}>
+      {/* <div className={`${style.circle} ${style.circle1}`} />
+      <div className={`${style.circle} ${style.circle2}`} /> */}
+      <Circle backgroundColor="#01c686" top="-45vh" left="-45vh" />
+      {/* <Circle backgroundColor="#01c686" right="-40vh" /> */}
+      <div className={style.card}>
+        <h1 className={style.title}>
+          <span className={style.brand}>GSHON</span> GSHON WE SOLUTIONS
+         
         </h1>
-        <p className={styles.desc}>
-          We provide all your web services in a professional mannner
+        <p className={style.desc}>
+          Create live segments and target the right people for messages based on
+          their behaviors.
         </p>
-        <button className={styles.button}>Discover</button>
+        <button className={style.button}>DISCOVER</button>
       </div>
-    <div className={style.card}>
+      <div className={style.card}>
         <Image
           src={process.env.NEXT_PUBLIC_URL + "/img/Avocado.png"}
           layout="fill"
